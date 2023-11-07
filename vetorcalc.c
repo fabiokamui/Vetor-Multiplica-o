1f82989b27742fdb712fa4e6dfdec092f7d2df96
Fabio Candido <fabio.silva910@etec.sp.gov.br>
2023-11-06 21:37:52 -0300

    Fabio

---
+++ vetorcalc.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (){
+
+int lista[10];
+int i;
+
+for(i=i<0;i<10;i=i+i){
+printf("Digite um numero para calcular%d:",i);
+scanf("%d", &lista[i]);
+lista[i]=lista[i] *2;
+	}
+
+printf("valores dobrados:\n");
+for (1=0; 1<10; i=i+1){
+	printf("posicao %d: %d\n", 1, lista[i]);
+}
+ return 0;
+
